aa5e1e8a1f2cd2f210b8d39bb13c967f0ca09edb
mzzdup-cmd <mzzdup@gmail.com>
2026-06-04 19:10:30 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,24 @@
 // Firebase Config
 
 const firebaseConfig = {
+
     apiKey: "AIzaSyChDLlxHqFbv6rsl5hyKRen3IME7da6c5U",
+
     authDomain: "sales-app-74357.firebaseapp.com",
+
     projectId: "sales-app-74357",
+
     storageBucket: "sales-app-74357.firebasestorage.app",
+
     messagingSenderId: "481008876572",
+
     appId: "1:481008876572:web:0a24f603311e1771f833d9"
 };
 
-// Инициализация Firebase
+// init firebase
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+// firestore
 
 const db = firebase.firestore();
-});
